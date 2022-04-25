1a58333e08028c7d7d63ba1fe113fde045e3d4df
王影 <wangying@everhigh.com.cn>
2022-04-25 13:33:48 +0800

    refactor(site): change pinia api url

---
+++ assets/Canvas.11a60782.js
@@ -0,0 +1,2 @@
+import{C as e}from"./index.97b55112.js";import{t as o,c as s,o as c}from"./vendor.b813516e.js";const l=o({setup(r){const t="https://coldcdn.com/api/cdn/v5ynur/ipfs/QmPTVdcP2HBcKcDZhV8TA4zjjdG65DW8Nc4rEFtM2Hr7nh";return(n,a)=>(c(),s(e,{cover:t,chain:"eth",name:"My Art",price:"0.00021 ETH",url:"https://google.com/#","logo-url":"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xB6eD7644C69416d67B522e20bC294A9a9B405B31/logo.png"}))}});export{l as default};
+//# sourceMappingURL=Canvas.11a60782.js.map
